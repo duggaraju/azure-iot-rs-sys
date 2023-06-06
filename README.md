227684266ba339786becd30d6038d1874b8e131c
Prakash Duggaraju <duggaraju@gmail.com>
2023-06-06 14:10:50 -0700

    Update to latest release tag of SDK. Add feature support

--- README.md
+++ README.md
@@ -2,21 +2,12 @@
 Rust bindings for azure_iot_sdk_c
 
 
-## Building 
-### Clone the minimum needed submodules.
-```
-git submodule update --init --depth 1
-cd azure-iot-sdk-c
-git submodule update --init --depth 1 umqtt/
-git submodule update --init --depth 1 uamqp/
-git submodule update --init --depth 1 c-utility/
-git submodule update --init --depth 1 deps/uhttp/
-git submodule update --init --depth 1 deps/umock-c/
-git submodule update --init --depth 1 deps/parson/
-git submodule update --init --depth 1 deps/azure-macrtoutils-c/
-
-```
-### Install the dependencies.
+# Building 
+## Install the dependencies.
 ```
 sudo apt-get install -y git cmake build-essential curl libcurl4-openssl-dev libssl-dev uuid-dev
+```
+##
+```
+cargo build
 ```
--- build.rs
+++ build.rs
@@ -5,6 +5,30 @@ use std::env;
 use std::path::PathBuf;
 use cmake;
 
+
+fn update_submodules(modules: &[&str], dir: &str) {
+    let mut args = vec![
+        "submodule",
+        "update",
+        "--init",
+        "--depth", "1",
+        "--recommend-shallow",
+    ];
+
+    args.extend_from_slice(modules);
+
+    let output = std::process::Command::new("git")
+        .current_dir(dir)
+        .args(args.into_iter())
+        .output()
+        .expect("Failed to update submodules");
+
+    if !output.status.success() {
+        panic!("Update submodules failed with status {:?}", output);
+    }
+
+}
+
 fn main() {
 
     // check for dependencies
@@ -12,23 +36,59 @@ fn main() {
     pkg_config::probe_library("openssl").unwrap();
     pkg_config::probe_library("libcurl").unwrap();
 
+    update_submodules(&["azure-iot-sdk-c"], ".");
+
+    let mut config = Config::new("azure-iot-sdk-c");
+    config
+    .define("use_edge_modules", "ON")
+    .define("skip_samples", "ON")
+    .define("CMAKE_C_FLAGS", "-Wno-array-parameter -Wno-deprecated-declarations -Wno-discarded-qualifiers");
+
+    let mut modules = vec![
+        "c-utility",
+        "deps/umock-c",
+        "deps/parson",
+        "deps/azure-macro-utils-c"
+    ];
+
+    if env::var_os("CARGO_FEATURE_AMQP").is_some() {
+        modules.push("uamqp/");
+        config.define("use_amqp", "ON");
+    } else {
+        config.define("use_amqp", "OFF");
+    }
+
+    if env::var_os("CARGO_FEATURE_MQTT").is_some() {
+        modules.push("umqtt");
+        config.define("use_mqtt", "ON");
+        println!("cargo:rustc-link-lib=umqtt");
+        println!("cargo:rustc-link-lib=iothub_client_mqtt_transport");
+    } else {
+        config.define("use_mqtt", "OFF");
+    }
+
+    if env::var_os("CARGO_FEATURE_HTTP").is_some() {
+        config.define("use_http", "ON");
+        modules.push("deps/uhttp/");
+        println!("cargo:rustc-link-lib=uhttp");
+    } else {
+        config.define("use_http", "OFF");
+    }
+
+    update_submodules(&modules, "azure-iot-sdk-c");
+
     // Builds the azure iot sdk, installing it
     // into $OUT_DIR
     use cmake::Config;
 
-    let dst = Config::new("azure-iot-sdk-c")
-                     .define("use_edge_modules", "ON")
-                     .build();
+    let dst = config.build();
     println!("cargo:rustc-link-search=native={}/lib", dst.display());
     
     // Tell cargo to tell rustc to link the azureiot libraries.
-    println!("cargo:rustc-link-lib=iothub_client_mqtt_transport");
     println!("cargo:rustc-link-lib=iothub_client");
     println!("cargo:rustc-link-lib=parson");
-    println!("cargo:rustc-link-lib=umqtt");
     println!("cargo:rustc-link-lib=prov_auth_client");
     println!("cargo:rustc-link-lib=hsm_security_client");
-    println!("cargo:rustc-link-lib=uhttp");
     println!("cargo:rustc-link-lib=aziotsharedutil");
     println!("cargo:rustc-link-lib=curl");
     println!("cargo:rustc-link-lib=ssl");
